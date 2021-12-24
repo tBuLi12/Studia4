33a50c64bbf333dcd88d069d6ce34109eb434d48
Karol Orzechowski <karol.orzechowski1337@gmail.com>
2021-12-24 14:32:26 +0100

    yes

--- studia4/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    exports com.papz22.studia4.gui;
-}
-module web {
-    requires com.zaxxer.hikari;
-    exports com.papz22.studia4.web;
-}
