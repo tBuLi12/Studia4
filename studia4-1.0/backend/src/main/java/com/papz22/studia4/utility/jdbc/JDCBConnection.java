0c7ea09a5eec53472a0838bc9f04939a621c82ce
Karol <karol.orzechowski@pw.edu.pl>
2022-01-12 04:28:23 +0100

    JDCBCcon clean up

--- studia4-1.0/backend/src/main/java/com/papz22/studia4/utility/jdbc/JDCBConnection.java
+++ studia4-1.0/backend/src/main/java/com/papz22/studia4/utility/jdbc/JDCBConnection.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.papz22.studia4.utility.exception.NotMatchingParameterException;
